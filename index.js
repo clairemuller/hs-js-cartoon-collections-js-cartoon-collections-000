bc6f530ff9599b9a56afff149d58d153cae8fbf9
Claire Muller <clairermuller@gmail.com>
2018-12-29 20:23:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
+  var string = "";
+  for (let i = 0; i < dwarves.length; i++) {
+    string += (`${i + 1}. ${dwarves[i]} `);
+  }
+  return string;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    arr.push(`${planeteerCalls[i]}!`.toUpperCase());
+  }
+  return arr;
 }
 
 function longPlaneteerCalls(words) {
+  var i = 0;
+  if (words[i].length > 4) {
+    return true;
+  } else if (words[i].length <= 4) {
+    return false;
+  }
 }
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar") {
+      return foods[i];
+    } else if (foods[i] === "gouda") {
+      return foods[i];
+    } else if (foods[i] === "camembert") {
+      return foods[i];
+    }
+  }
+  return 'no cheese!';
 }
+
